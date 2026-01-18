9cf08ed0c4b9b90130dc00d888bfe0c93842a93e
Aleksei Novikov <novikov.aleksei.92@gmail.com>
2026-01-18 16:14:35 +0000

    Fixes

--- src/platform.rs
+++ src/platform.rs
@@ -1,8 +1,9 @@
 #[cfg(windows)]
 mod windows {
+    use windows_sys::Win32::Graphics::Dwm::{DwmExtendFrameIntoClientArea, MARGINS};
     use windows_sys::Win32::UI::WindowsAndMessaging::{
-        GWL_EXSTYLE, GetWindowLongW, SetWindowLongW, WS_EX_LAYERED, WS_EX_NOACTIVATE,
-        WS_EX_TOOLWINDOW, WS_EX_TRANSPARENT,
+        GWL_EXSTYLE, GetWindowLongW, LWA_ALPHA, SetLayeredWindowAttributes, SetWindowLongW,
+        WS_EX_LAYERED, WS_EX_NOACTIVATE, WS_EX_TOOLWINDOW, WS_EX_TRANSPARENT,
     };
     use winit::raw_window_handle::{HasWindowHandle, RawWindowHandle};
 
@@ -20,6 +21,14 @@ mod windows {
             let new_style = ex_style
                 | (WS_EX_LAYERED | WS_EX_TRANSPARENT | WS_EX_TOOLWINDOW | WS_EX_NOACTIVATE) as i32;
             let _ = SetWindowLongW(hwnd, GWL_EXSTYLE, new_style);
+            let _ = SetLayeredWindowAttributes(hwnd, 0, 255, LWA_ALPHA);
+            let margins = MARGINS {
+                cxLeftWidth: -1,
+                cxRightWidth: 0,
+                cyTopHeight: 0,
+                cyBottomHeight: 0,
+            };
+            let _ = DwmExtendFrameIntoClientArea(hwnd, &margins);
         }
     }
 }
